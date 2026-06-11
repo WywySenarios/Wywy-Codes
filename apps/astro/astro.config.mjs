f37c12299d142ddbc00b1f263979a57bf237c058
Eric Zhu <wywyericzhu@gmail.com>
2026-06-11 08:18:58 -0400

    fix: dev server ALLOWED_HOSTS

--- apps/astro/astro.config.mjs
+++ apps/astro/astro.config.mjs
@@ -2,10 +2,17 @@ import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
 import tailwindcss from "@tailwindcss/vite";
 
+const allowedHosts = process.env.ALLOWED_HOSTS
+  ? process.env.ALLOWED_HOSTS.split(",")
+  : undefined;
+
 export default defineConfig({
   output: "static",
   integrations: [react()],
   vite: {
     plugins: [tailwindcss()],
+    server: {
+      allowedHosts,
+    },
   },
 });
